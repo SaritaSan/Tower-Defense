329b9ac2de9171fe8262545370e3993ad9ac3125
SaritaSan <156473953+SaritaSan@users.noreply.github.com>
2025-07-18 18:36:05 -0600

    Avance 4
    Muchas cosas XD

---
+++ Assets/Scripts/EnemyData.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "EnemyData", menuName = "Scriptable Objects/EnemyData")]
+public class EnemyData : ScriptableObject
+{
+    public float attackRange = 1f;
+    public float attackDamage = 10f;
+    public float runSpeed = 2f;
+    public float attackDuration = 1f;
+    public float attackCooldown = 1f;
+    public string primaryTargetTag = "Tower";
+    public string runAnimationName = "ZombieRun";
+    public string attackAnimationName = "ZombieAttack";
+    public string dieAnimationName = "ZombieDie";
+}
